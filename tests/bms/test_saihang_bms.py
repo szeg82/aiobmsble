7ae42546d0ba920e3330cf025e81e95fde96218c
Szegedi Istvan <istvan.szegedi@gmail.com>
2026-05-01 21:48:47 +0300

    Update mock responses and extend telemetry fields in Saihang BMS tests.

--- tests/bms/test_saihang_bms.py
+++ tests/bms/test_saihang_bms.py
@@ -24,13 +24,14 @@ class MockSaihangBleakClient(MockBleakClient):
     """Emulate a Saihang BMS BleakClient."""
 
     _RESP: Final[bytes] = (
-        b"\xa5\xa5\x00\x03\x90\x00\x00\x00\x00\x00\x00\x0a\xa8\x00\x60\x00\x64\x00\x00\x25\xa6\x00"
+        b"\xa5\xa5\x00\x03\xa6\x00\x00\x00\x00\x00\x00\x0a\xa8\x00\x60\x00\x64\x00\x00\x25\xa6\x00"
         b"\x00\x27\x10\x00\x00\x27\x10\x00\x02\xff\xff\x00\x01\x00\x00\x06\x00\x00\x00\x00\x00\x00"
         b"\x08\x0d\xa9\x0d\x6a\x0d\x28\x0d\x3d\x0d\x2c\x0d\x6b\x0d\x4d\x0d\x38\xff\xff\xff\xff\xff"
         b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x02\x0b"
         b"\x43\x0b\x46\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x0b\x56\x0b"
-        b"\xa4\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\x6d\x60\x00\x00\x72"
-        b"\x10\x00\x00\x68\x10\x00\x0a\x0d\xac\x0e\x42\x0d\x02\x00\x0a\x00\x00\x9a\x5a"
+        b"\xa4\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x00\x00\xdd\xe0\x00\x00\xe1"
+        b"\x00\x00\x00\xd3\x40\x00\x0a\x0e\x10\x0e\x42\x0d\x34\x00\x0a\x00\x00\xb3\xb0\x00\x00\xaf"
+        b"\xc8\x00\x00\xbb\x80\x00\x0a\x0b\x54\x0a\xf0\x0b\xb8\x00\x0a\xff\xff\xe8\xe5"
     )
 
     async def write_gatt_char(
@@ -47,7 +48,7 @@ class MockSaihangBleakClient(MockBleakClient):
             self._notify_callback
         ), "write to characteristics but notification not enabled"
 
-        if bytes(data) != b"\xa5\xa5\x00\x03\x00\x00\x00\x48\x44\x2d":
+        if bytes(data) != b"\xa5\xa5\x00\x03\x00\x00\x00\x53\x04\x26":
             return
 
         self._notify_callback("MockRoyPowBleakClient", self._RESP)
@@ -69,8 +70,8 @@ async def test_update(patch_bleak_client, keep_alive_fixture: bool) -> None:
         "cell_count": 8,
         "current": 0.0,
         "cycles": 2,
-        "temp_sensors": 2,
-        "temp_values": [15.2, 15.5],
+        "temp_sensors": 4,
+        "temp_values": [15.2, 15.5, 17.2, 25.0],
         "cell_voltages": [
             3.497,
             3.434,
@@ -83,8 +84,28 @@ async def test_update(patch_bleak_client, keep_alive_fixture: bool) -> None:
         ],
         "cycle_capacity": 2629.246,
         "cycle_charge": 96.38,
+        "total_charge": 10.0,
         "delta_voltage": 0.129,
-        "design_capacity": 100,
+        "design_capacity": 100.0,
+        "balancer": 6,
+        "chrg_mosfet": False,
+        "dischrg_mosfet": False,
+        "pack_ov_alarm": 56.8,
+        "pack_ov_protection": 57.6,
+        "pack_ov_release": 54.08,
+        "pack_ov_delay": 1.0,
+        "cell_ov_alarm": 3.6,
+        "cell_ov_protection": 3.65,
+        "cell_ov_release": 3.38,
+        "cell_ov_delay": 1.0,
+        "pack_uv_alarm": 46.0,
+        "pack_uv_protection": 45.0,
+        "pack_uv_release": 48.0,
+        "pack_uv_delay": 1.0,
+        "cell_uv_alarm": 2.9,
+        "cell_uv_protection": 2.8,
+        "cell_uv_release": 3.0,
+        "cell_uv_delay": 1.0,
         "problem": False,
         "problem_code": 0,
         "power": 0.0,
@@ -106,7 +127,7 @@ def test_uuid_tx_not_used() -> None:
     ("wrong_response"),
     [
         b"",
-        b"\xa5\xa5\x00\x03\x90\x71\x5c",
+        b"\xa5\xa5\x00\x03\xa6\x71\x5c",
         b"\xa5\xa6" + MockSaihangBleakClient._RESP[2:],
         MockSaihangBleakClient._RESP[:-2] + b"\x00\x00",
         b"\xa5\xa5\x00\x03\x89" + MockSaihangBleakClient._RESP[5:-2] + b"\xfc\x79",
